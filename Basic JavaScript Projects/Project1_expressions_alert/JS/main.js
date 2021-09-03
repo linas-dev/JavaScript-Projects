e47aa12a9593bb8629a2006a9a10f4d883f91d4c
Linas Smita <linassmita@Linass-MacBook-Air.local>
2021-09-03 22:30:09 +0100

    my first function

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -10,29 +10,38 @@
 // var color = color.fontcolor("blue");
 
 // document.write(`My favorite color is: ${color}`);
-var dad = "👴";
-var mom = "👵";
-var brother = "👨";
-var sister = "👩";
+// var dad = "👴";
+// var mom = "👵";
+// var brother = "👨";
+// var sister = "👩";
 
 
-var myDadColor = "Green";
-var myDadColor = myDadColor.fontcolor("green");
+// var myDadColor = "Green";
+// var myDadColor = myDadColor.fontcolor("green");
 
-var myMomColor = "Blue";
-var myMomColor = myMomColor.fontcolor("blue")
+// var myMomColor = "Blue";
+// var myMomColor = myMomColor.fontcolor("blue")
 
-var mySisterColor = "Red";
-var mySisterColor = mySisterColor.fontcolor("red");
+// var mySisterColor = "Red";
+// var mySisterColor = mySisterColor.fontcolor("red");
 
-var myBrotherColor = "Brown";
-var myBrotherColor = myBrotherColor.fontcolor("brown")
+// var myBrotherColor = "Brown";
+// var myBrotherColor = myBrotherColor.fontcolor("brown")
 
-document.write(`
-    <h2> My ${dad} dads fav color is: ${myDadColor} </br> </h2>
-    <h2> My ${mom} moms fav color is:  ${myMomColor} </br> </h2>
-    <h2> My ${brother} brothers fav color is:  ${myBrotherColor} </br> </h2>
-    <h2> My ${sister} sisters fav color is:  ${mySisterColor} </h2>
-`);
+// document.write(`
+//     <h2> My ${dad} dads fav color is: ${myDadColor} </br> </h2>
+//     <h2> My ${mom} moms fav color is:  ${myMomColor} </br> </h2>
+//     <h2> My ${brother} brothers fav color is:  ${myBrotherColor} </br> </h2>
+//     <h2> My ${sister} sisters fav color is:  ${mySisterColor} </h2>
+// `);
 
+
+// document.write(3+3);
+
+
+function My_First_Function(){
+    var letters = "This text is blue";
+    var result = letters.fontcolor("blue");
+    document.getElementById("blueText").innerHTML = result;
+};
 
