ff9b4aafe094962f9ee61ee3be6408a83d88ed92
Linas Smita <linassmita@Linass-MacBook-Air.local>
2021-09-03 21:55:54 +0100

    str.fontcolor()

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -1,6 +1,12 @@
-const myDad = "Tom";
-const myMom = "Laura";
-const mySister = "Simona";
-const myBrother = "Zvaigzdzius";
+// const myDad = "Tom";
+// const myMom = "Laura";
+// const mySister = "Simona";
+// const myBrother = "Zvaigzdzius";
 
-document.write(`My dad is ${myDad} my mom is ${myMom} my brother is ${myBrother} my sister is ${mySister}`);
+// document.write(`My dad is ${myDad} my mom is ${myMom} my brother is ${myBrother} my sister is ${mySister}`);
+
+
+var color = "blue"; 
+var color = color.fontcolor("blue");
+
+document.write(`My favorite color is: ${color}`);
