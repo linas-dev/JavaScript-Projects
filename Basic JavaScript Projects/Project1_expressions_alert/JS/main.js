1dd5d314a1904af64c6b76d8eccb1df57deceeaf
Linas Smita <linassmita@Linass-MacBook-Air.local>
2021-09-03 21:45:39 +0100

    assigning multiple variables in one statement

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -1,3 +1,6 @@
-const myName = "Linas";
+const myDad = "Tom";
+const myMom = "Laura";
+const mySister = "Simona";
+const myBrother = "Zvaigzdzius";
 
-window.alert(myName);
+document.write(`My dad is ${myDad} my mom is ${myMom} my brother is ${myBrother} my sister is ${mySister}`);
