00f476d8cc60999d0a57450d042ffdabca36df6e
Linas Smita <linassmita@Linass-MacBook-Air.local>
2021-09-03 22:04:17 +0100

    assign colors to all of the variables

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -6,7 +6,28 @@
 // document.write(`My dad is ${myDad} my mom is ${myMom} my brother is ${myBrother} my sister is ${mySister}`);
 
 
-var color = "blue"; 
-var color = color.fontcolor("blue");
+// var color = "blue"; 
+// var color = color.fontcolor("blue");
 
-document.write(`My favorite color is: ${color}`);
+// document.write(`My favorite color is: ${color}`);
+
+var myDadColor = "Green";
+var myDadColor = myDadColor.fontcolor("green");
+
+var myMomColor = "Blue";
+var myMomColor = myMomColor.fontcolor("blue")
+
+var mySisterColor = "Red";
+var mySisterColor = mySisterColor.fontcolor("red");
+
+var myBrotherColor = "Brown";
+var myBrotherColor = myBrotherColor.fontcolor("brown")
+
+document.write(`
+    My dads fav color is: ${myDadColor}</br> 
+    My dads fav color is:  ${myMomColor} </br> 
+    My dads fav color is:  ${myBrotherColor} </br>
+    My dads fav color is:  ${mySisterColor}
+    `);
+
+
