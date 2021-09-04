7898abf8b73137e84eb0e58e8e1d96b4e21fea25
Linas Smita <linassmita@Linass-MacBook-Air.local>
2021-09-04 15:46:54 +0100

    +=  operator to concatenate a string

--- Basic JavaScript Projects/Project2_functions/JS/main.js
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -1,17 +1,29 @@
-function getExamples() {
-    var getexample1 = "Example changed";
-    var getexample2 = getexample1.fontcolor("blue")
-    document.getElementById("example1").innerHTML = getexample2;
+// function getExamples() {
+//     var getexample1 = "Example changed";
+//     var getexample2 = getexample1.fontcolor("blue")
+//     document.getElementById("example1").innerHTML = getexample2;
 
-    var getexample3 = `Example changed 2`;
-    var getexample4 = getexample3.fontsize("100px");
-    document.getElementById("example2").innerHTML = getexample4;
+//     var getexample3 = `Example changed 2`;
+//     var getexample4 = getexample3.fontsize("100px");
+//     document.getElementById("example2").innerHTML = getexample4;
+// };
+
+// function addMore(){
+//     var button = document.createElement("button");
+//     button.innerHTML = "Added more buttons 😅";
+
+//     var div = document.getElementById("mb");
+//     div.appendChild(button);
+// };
+
+
+function clickFunction(){
+    var changeInnerText = "Text was changed ";
+    var surpriseText = "This new blue color text"
+    var surpriseText = surpriseText.fontcolor("blue");
+    var surpriseText = surpriseText.fontsize("5");
+    changeInnerText += ` to: ${surpriseText} `;
+
+    document.getElementById("click").innerHTML = changeInnerText;
 };
 
-function addMore(){
-    var button = document.createElement("button");
-    button.innerHTML = "Added more buttons 😅";
-
-    var div = document.getElementById("mb");
-    div.appendChild(button);
-};
