2b6a5543a5b72ff30f43bd76119b4b72c36b4144
Linas Smita <linassmita@Linass-MacBook-Air.local>
2021-09-04 15:36:33 +0100

    added my own examples

--- Basic JavaScript Projects/Project2_functions/JS/main.js
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -1,1 +1,17 @@
-window.alert("linas");
+function getExamples() {
+    var getexample1 = "Example changed";
+    var getexample2 = getexample1.fontcolor("blue")
+    document.getElementById("example1").innerHTML = getexample2;
+
+    var getexample3 = `Example changed 2`;
+    var getexample4 = getexample3.fontsize("100px");
+    document.getElementById("example2").innerHTML = getexample4;
+};
+
+function addMore(){
+    var button = document.createElement("button");
+    button.innerHTML = "Added more buttons 😅";
+
+    var div = document.getElementById("mb");
+    div.appendChild(button);
+};
