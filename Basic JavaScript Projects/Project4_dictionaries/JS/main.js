8dc757fa99324df44969be2f779bbc5df3a7b9ad
Linas Smita <linassmita@Linass-MacBook-Air.local>
2021-09-20 11:45:38 +0100

    dictionary

--- Basic JavaScript Projects/Project4_dictionaries/JS/main.js
+++ Basic JavaScript Projects/Project4_dictionaries/JS/main.js
@@ -1,23 +1,34 @@
-// function addition(a,b) {
-//     return a + b;
-// }
-// document.write(addition(6,8))
+function getPair() {
+    var Car = {
+        Brand: "Mercedes",
+        Model: "AMG",
+        Convertable: false,
+        Doors: 5,
+        Engine: "V12",
+        Speed: "500 M/H"
+    };
 
-// function subtract(a,b) {
-//     return a - b;
-// }
-// document.write(subtract(6,8))
+    var pair = document.getElementById("key-value-pair").innerHTML += ` 
+    <p>Car Brand: ${Car.Brand} </p> </br>
+    <p>Car Model: ${Car.Model} </p> </br>
+    <p>Is convertable?: ${Car.Convertable} </p> </br>
+    <p>Doors: ${Car.Doors} </p> </br>
+    <p>Car Engine Size: ${Car.Engine} </p> </br>
+    <p>Car Speed: ${Car.Speed} </p>
+    `;
+};
 
+function getPair2() {
+    var Car = {
+        Brand: "Mercedes",
+        Model: "AMG",
+        Convertable: false,
+        Doors: 5,
+        Engine: "V12",
+        Speed: "500 M/H"
+    };
 
-// function multiply(a,b) {
-//     return a * b;
-// }
-// document.write(multiply(6,8))
-
-// function multiply(a,b) {
-//     return a % b;
-// }
-// document.write(multiply(6,8))
-
-
-
+    delete Car.Model;
+    var pair = document.getElementById("key-value-pair2").innerHTML += ` 
+    <p>${Car.Model} </p>`;
+};
